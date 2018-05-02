7b7ec2e97feb023d08dd0a7ae5feed079f7b709f
Slawa Konkevych <konkevych@hotmail.com>
2018-05-02 17:17:40 +0200

    Current State
    Current state

---
+++ Run.bat
@@ -0,0 +1,2 @@
+java -jar target/TextSecureServer-1.68.jar server config/signal-server.yml
+pause
---
+++ config/self-signed-cert-gen.cmd
@@ -0,0 +1,2 @@
+"C:\Program Files\Java\jdk1.8.0_162\bin\keytool.exe" -genkeypair -keyalg RSA -dname "CN=intellion.fritz.box" -keystore host.keystore -keypass pa$$word -storepass pa$$word
+pause
